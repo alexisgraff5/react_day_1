595af2d6a8db6db33d6d4d95639daa9b3a9244ad
alexisgraff5 <lex.graff@gmail.com>
2016-12-12 11:21:19 -0700

    learning react

--- app/App.component.js
+++ app/App.component.js
@@ -1,12 +1,60 @@
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 import React from 'react';
 
 var App = React.createClass({
+  getInitialState() {
+    return {
+      name: 'lex',
+      email: 'lex.graff@gmail.com',
+      friends: [
+        {name: 'brian'},
+        {name: 'sterling'},
+        {name: 'gustav'},
+        {name: 'josh'},
+        {name: 'elisha'},
+      ]
+    };
+  },
   render() {
     return (
-      <div>I'm react</div>
+      <div>
+        {this.state.name}
+        <button onClick={this.clickHandler}>
+          Update State
+        </button>
+        <input onChange={this.inputHandler}/>
+        {this.state.friends
+          .filter(friend => {
+            return friend.name !== 'josh'
+          })
+          .map((friend) => {
+          return <div
+            onClick={this.friendNamer.bind(this, friend.name)}
+            key={friend.name}>
+            {friend.name}
+          </div>
+        })}
+      </div>
     )
-  }
+  },
+  inputHandler(event) {
+    this.setState({
+      name: event.target.value,
+    })
+  },
+
+  clickHandler() {
+    this.setState({
+      name: 'bob'
+    });
+  },
+
+  friendNamer(name) {
+    this.setState({
+      name: name,
+    });
+  },
+
 })
 
 ReactDOM.render(<App />, document.getElementById("app"));
